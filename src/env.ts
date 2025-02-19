b92b84c0c0fb46deb880e2e0b28604640e9b425d
OJailson17 <JaylsonO17@gmail.com>
2025-02-18 22:48:52 -0300

    feat: config db and add subscribe to event feat

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,9 @@
 import z from 'zod';
 
 const envSchema = z.object({
-	PORT: z.coerce.number().default(8082),
+  PORT: z.coerce.number().default(8082),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
